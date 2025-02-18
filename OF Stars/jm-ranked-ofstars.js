260012052f11c8eee0831bfe2d8ee371718582cf
CaesarStud <julaenw@gmail.com>
2025-02-18 17:27:55 -0500

    Update jm-ranked-ofstars.js

--- OF Stars/jm-ranked-ofstars.js
+++ OF Stars/jm-ranked-ofstars.js
@@ -84,6 +84,7 @@ function handleGiveUp(){
         giveUp.style.display = 'none'; // Hide the Give Up button
         clearInterval(timerInterval); // Stop the timer
         playAgain.style.display = 'block';
+        triggerConfetti();
     } else {
         // Move to the next image after 1 second
         setTimeout(() => {
@@ -117,6 +118,15 @@ function displayRandomImage() {
     remainingImages.splice(randomIndex, 1);
 }
 
+// Function to trigger confetti
+function triggerConfetti(){
+    confetti({
+        particleCount: 100, // Number of confetti particles
+        spread: 70, // How far the particles spread
+        origin: { y: 0.6} // Start confetti from the bottom of the screen
+    });
+}
+
 // Function to check if the guess is correct
 function checkGuess() {
     const userGuess = guessInput.value.trim().toLowerCase(); // Get the user's guess
@@ -138,6 +148,7 @@ function checkGuess() {
             giveUp.style.display = 'none'; // Hide the Give Up button
             clearInterval(timerInterval); // Stop the timer
             playAgain.style.display = 'block';
+            triggerConfetti();
         }
     } else {
         feedback.textContent = "Incorrect! 😢";
