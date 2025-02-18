c85a3739a95b729e0f339fc3fb2cad151d3fd706
CaesarStud <julaenw@gmail.com>
2025-02-18 17:22:57 -0500

    Update jm-ranked-brunette.js

--- Brunette/jm-ranked-brunette.js
+++ Brunette/jm-ranked-brunette.js
@@ -116,6 +116,7 @@ function handleGiveUp(){
         giveUp.style.display = 'none'; // Hide the Give Up button
         clearInterval(timerInterval); // Stop the timer
         playAgain.style.display = 'block';
+        triggerConfetti();
     } else {
         // Move to the next image after 1 second
         setTimeout(() => {
@@ -149,6 +150,15 @@ function displayRandomImage() {
     remainingImages.splice(randomIndex, 1);
 }
 
+// Function to trigger confetti
+function triggerConfetti(){
+    confetti({
+        particleCount: 100, // Number of confetti particles
+        spread: 70, // How far the particles spread
+        origin: { y: 0.6} // Start confetti from the bottom of the screen
+    });
+}
+
 // Function to check if the guess is correct
 function checkGuess() {
     const userGuess = guessInput.value.trim().toLowerCase(); // Get the user's guess
@@ -170,6 +180,7 @@ function checkGuess() {
             giveUp.style.display = 'none'; // Hide the Give Up button
             clearInterval(timerInterval); // Stop the timer
             playAgain.style.display = 'block';
+            triggerConfetti();
         }
     } else {
         feedback.textContent = "Incorrect! 😢";
